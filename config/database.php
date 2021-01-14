4bb327bf1ca05fe25c7834419987b957068c7cda
Lester Hurtado <lester.hurtado@Lesters-MacBook-Pro.local>
2021-01-15 00:30:06 +0800

    update database.php

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url("postgres://mwmxziuoreneoe:37b6a67d82c94981c443a67b6f1a84dd3a9bbf1049399a577bf4e6a0e3ebb16d@ec2-54-144-196-35.compute-1.amazonaws.com:5432/d843vj12mgkhbu");
+
 return [
 
     /*
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'],"/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
